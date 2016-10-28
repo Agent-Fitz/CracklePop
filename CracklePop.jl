ab92e3810c1f0386ac160ac0600d7231337433f6
aif <ameliafitzsimmons1@gmail.com>
2016-10-27 22:01:15 -0400

    initial commit of cracklepop.jl

---
+++ CracklePop.jl
@@ -0,0 +1,9 @@
+julia> function CracklePop()
+       for i in 1:100
+         if rem(i, 3) == 0 && rem(i, 5) == 0 println("CracklePop")
+         elseif rem(i, 5) == 0 println("Pop")
+         elseif rem(i, 3) == 0 println("Crackle")
+        else println(i)
+        end
+      end
+end
