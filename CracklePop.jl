9865899b69ab03d49e9be6d003ba253c621a9a03
Amelia <aif@ornl.gov>
2016-10-28 16:12:49 -0400

    removed ugly formatting in .jl file

--- CracklePop.jl
+++ CracklePop.jl
@@ -1,4 +1,4 @@
-julia> function CracklePop()
+function CracklePop()
        for i in 1:100
          if rem(i, 3) == 0 && rem(i, 5) == 0 println("CracklePop")
          elseif rem(i, 5) == 0 println("Pop")
